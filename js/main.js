87c18478048d575af4d966a759e8c3c53c75e0fc
Chris G <cgardn@users.noreply.github.com>
2023-06-10 00:01:11 -0400

    rough draft new site
    basic layout for mobile+desktop
    - missing styles for everything
    - missing fonts
    - no background
    - mobile menu is very unstyled
    
    also added basic slide transitions for content to both mobile+desktop

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// changing root variables for switching light and dark 
+// document.querySelector(':root').style.setProperty('--background', 'var(--light-background)')
+
+// elements
+const mobileMenu = document.getElementById("mobile-menu");
+const mobileMenuToggle = document.getElementById("mobile-menu-toggle");
+const mainContent = document.querySelector("main");
+
+function toggleMobileMenu() {
+	if (mobileMenu) {
+		mobileMenu.classList.toggle("active")
+	}
+}
+
+// mobile menu toggle
+if (mobileMenuToggle) { mobileMenuToggle.addEventListener("click", toggleMobileMenu) }
+
+// conditional click on mobile page for closing mobile menu
+if (mainContent) {
+	mainContent.addEventListener('click', () => {
+		if (mobileMenu && mobileMenu.classList.contains('active')) { toggleMobileMenu() }
+	})
+}
+
+// slide controls
+const menus = [document.getElementById("desktop-menu"), document.getElementById("mobile-menu")];
+menus.forEach(menu => {
+	if (menu) {
+		menu.addEventListener("click", (e) => {
+			if (e.target.classList.contains('menu-item') && Object.keys(e.target.dataset).includes('slide')) {
+				const slideTargets = document.querySelectorAll('.content-container .content')
+				slideTargets.forEach(el => el.style.transform = `translateX(-${Number(e.target.dataset.slide)*100}%`)
+			}
+		})
+	}
+})
