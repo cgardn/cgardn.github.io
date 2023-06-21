2116c5886a756a5b49e9701085034c3ae76387f3
Chris G <cgardn@users.noreply.github.com>
2023-06-20 21:34:58 -0400

    final stretch
    added menu + styled everything, including fade-out of navbar when going back to home
    
    also replaced portfolio with a bunch more stuff from LBP work

--- js/main.js
+++ js/main.js
@@ -30,38 +30,22 @@ if (mainContent) {
 }
 
 // slide controls
-const menus = [document.getElementById("desktop-menu"), document.getElementById("mobile-menu")];
+const menus = [document.getElementById("desktop-menu"), document.getElementById("mobile-menu"), document.querySelector('header')];
 menus.forEach(menu => {
 	if (menu) {
 		menu.addEventListener("click", (e) => {
 			e.preventDefault()
-			console.log(e)
-			document.querySelectorAll('.fade-slide-in').forEach(el => {
-				el.classList.add('fade-slide-out');
-			})
-			setTimeout(() => {window.location.href = e.target.parentElement.href}, 350)
-			
-		})
-		/*
-			const currentSlide = document.querySelector('[data-current-slide="true"]');
-			if (e.target.dataset.slide == currentSlide.dataset.slide) { return }
-			if (e.target.classList.contains('menu-item') && Object.keys(e.target.dataset).includes('slide')) {
-				const slideTargets = document.querySelectorAll('.content-container .content')
-				slideTargets.forEach(el => {
-					if (el.dataset.slide == e.target.dataset.slide) { el.dataset.currentSlide = 'true' }
-					else { el.dataset.currentSlide = 'false' }
-					el.classList.remove("active")
-					el.classList.add("inactive")
-					setTimeout( () => {
-						// this movement should only be happening when all slides are opacity 0
-						el.style.transform = `translateX(-${Number(e.target.dataset.slide)*100}%`
-					}, 300) // make sure this matches the CSS animation timing for content-in and content-out, later get from JS CSSKeyframesRule interface when remote hosted (cors blocks js access of stylesheets)
-					setTimeout( () => {
-						el.classList.add("active")
-						el.classList.remove("inactive")
-					}, 300)
+			if (e.target.parentElement.tagName === 'A') {
+				if (new URL(e.target.parentElement.href).pathname === '/') {
+					document.querySelectorAll('header a').forEach(el =>  {
+						el.classList.add('fade-slide-out')
+					})
+				}
+				document.querySelectorAll('.fade-slide-in').forEach(el => {
+					el.classList.add('fade-slide-out');
 				})
+				setTimeout(() => {window.location.href = e.target.parentElement.href}, 350)
 			}
-		})*/
+		})
 	}
 })
