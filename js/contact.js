9b5b1e5e34bde87fbeffe75ef5440be472bc0e6c
Chris G <cgardn@users.noreply.github.com>
2023-08-22 20:45:50 -0400

    fix style issues, add contact form
    - added contact form with FormSpark
    - fixed font load flashing with a preload
    - some other minor style adjustments (mobile width, etc)

--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,6 @@
 // submit contact form
 const form = document.getElementById("contact-form");
+const postMessage = document.getElementById(".post-submit-message");
 form.querySelector("button").addEventListener("click", submitForm)
 
 async function submitForm(e) {
@@ -10,17 +11,23 @@ async function submitForm(e) {
 	try {
 		let json = {}
 		for ([key, value] of new FormData(form).entries()) { obj[key] = value };
-		const response = await fetch("https://professional-site-1662.twil.io/form", {
+		const response = await fetch("https://submit-form.com/b79R1d88", {
 			method: "POST",
 			headers: {
-				"content-type": "application/json"
+				"content-type": "application/json",
+				Accept: "application/json"
 			},
 			body: JSON.stringify(json)
 		})
 		const data = await response.json()
 		if (response.status && response.status != 200) { throw new Error("Submission failed") }
 		form.querySelectorAll("input, textarea, button").forEach(el => el.disabled = false);
-		form.querySelector("button").innerText = "Submit"
+		form.querySelector("button").innerText = "Submit";
+		form.classList.add("fade-slide-out");
+		window.setTimeout( () => {
+			elementsToHide(.forEach(
+			postMessage.classList.add("fade-slide-in");
+		}, 350);
 	} catch (err) {
 		console.log(err);
 		form.querySelectorAll("input, textarea, button").forEach(el => el.disabled = false);
